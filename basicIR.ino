aef5fa07e92c42ac82b4a648f2a1a1cf8bf40ce0
zinp5 <159497139+zinp5@users.noreply.github.com>
2025-01-10 20:31:17 -0800

    apparently it uses LOW to detect motion
    updated code accordingly
    also added currstatus variable to read status from pin

--- basicIR.ino
+++ basicIR.ino
@@ -1,17 +1,18 @@
 //#include <Arduino.h>
 
 const int IRpin = 33;       //set the input pin
-
+int currstatus;             //holds the status of the IR sensor
 void setup() {
   pinMode(IRpin, INPUT);    //set pinmode for IR pin to input
   Serial.begin(9600);       //open serial monitor for basic output
 }
 
 void loop() {
-  if(IRpin == HIGH){                   //if input pin says high
-    Serial.write("Detected\n");
+  currstatus = digitalRead(IRpin);
+  if(currstatus == LOW){                   //if input pin says LOW
+    Serial.write("{=}+");
+    
   }
-  else{
-    Serial.write("---\n");            //if input pin says low
-  }
+  currstatus = LOW;
+  
 }
